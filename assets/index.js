e2ef23e3c363cb9e619abcb074194755b31b7a4d
NWeze Daniel <flashdaniel0@gmail.com>
2024-10-13 18:07:58 +0100

    create move on click button carousel

---
+++ assets/index.js
@@ -0,0 +1,27 @@
+function scrollCarousel() {
+  const carousel = document.querySelector(".carousel");
+  const carouselScrollPart = carousel.children[0];
+
+  [...carousel.children[1].children].forEach((button) => {
+    button.addEventListener("click", () => {
+      const element1 = carouselScrollPart.children[0];
+      const element2 = carouselScrollPart.children[1];
+      let scrollCount =
+        element1.offsetLeft +
+        element1.offsetWidth +
+        element2.offsetLeft +
+        element2.offsetWidth +
+        70;
+
+      if (button.textContent == "⇐") {
+        carouselScrollPart.scrollLeft -= scrollCount;
+      }
+
+      if (button.textContent == "⇒") {
+        carouselScrollPart.scrollLeft += scrollCount;
+      }
+    });
+  });
+}
+
+scrollCarousel();
